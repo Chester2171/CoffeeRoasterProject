076caf0959278ed42630c6ef13888eefb08f32eb
Chester2171 <Chester2171@gmail.com>
2020-08-29 15:45:20 -0700

    Inital Application

---
+++ NightSightsCoffee/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NightSightsCoffee.Models;
+
+namespace NightSightsCoffee.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("Cart")]
+        public IActionResult AddToCart()
+        {
+            return View();
+        }
+
+        [HttpGet("Contact")]
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+
+        [HttpGet("Story")]
+        public IActionResult OurStory()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
