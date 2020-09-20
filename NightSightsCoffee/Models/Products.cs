fe88bcc32ba12b6e60c54153ac1930ea98ffcc9f
Chester2171 <Chester2171@gmail.com>
2020-09-19 21:14:58 -0700

    Changes

---
+++ NightSightsCoffee/Models/Products.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NightSightsCoffee.Models
+{
+    public class Products
+    {
+    }
+}
